805c12a89f0bd7086fd2465c3370d7a27afc96c5
Víctor J. Jara <victorjjara@gmail.com>
2025-06-29 12:56:18 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
